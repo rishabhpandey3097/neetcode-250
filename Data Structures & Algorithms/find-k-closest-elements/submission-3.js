966c8dca368281ed3254548253e0f0356c1fe880
Rishabh Pandey <84588918+rishabhpandey3097@users.noreply.github.com>
2026-04-10 17:51:38 +0530

    Add: find-k-closest-elements - submission-3

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @param {number} k
+     * @param {number} x
+     * @return {number[]}
+     */
+    findClosestElements(arr, k, x) {
+        let l = 0;
+        let r = arr.length - 1;
+
+        while(r - l >= k) {
+            if(Math.abs(x - arr[l]) <= Math.abs(x - arr[r])) {
+                r--
+            }else {
+                l++
+            }
+        }
+
+        return arr.slice(l, r + 1)
+    }
+}
