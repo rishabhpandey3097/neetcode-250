ecabebaaf6f39c25e9b6eef76d60bf5178ab45ac
Rishabh Pandey <84588918+rishabhpandey3097@users.noreply.github.com>
2026-04-11 14:23:35 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        let stack = [];
+
+        for(let op of operations) {
+            if(op === '+') {
+                const top = stack.pop();
+                const newTop = top + stack.at(-1); // at(-1) ==> [length-1]
+                stack.push(top);
+                stack.push(newTop);
+            }else if (op === 'C') {
+                stack.pop()
+            }else if(op === 'D') {
+                let double = stack.at(-1) * 2;
+                stack.push(double)
+            }else {
+                stack.push(parseInt(op))
+            }
+        }
+
+        return stack.reduce((acc, curr) => acc + curr, 0);
+    }
+}
